430cc31adc1267303359a65b28f30e63f6aa14f3
nikitaNaredi <neketa.kumare123gmail.com>
2019-08-07 11:08:28 +0530

    Detail Api call on button

--- front-end-codes/project-git/main.js
+++ front-end-codes/project-git/main.js
@@ -1,32 +1,47 @@
  
- function detail(i){
+ function detail(i,url){
+	 getname(i,url);
 	 document.getElementsByClassName("right-content")[i].style.display = "none";
 	 	 document.getElementsByClassName("detail-data")[i].style.display = "block";
 		 document.getElementsByClassName("right-content-collapse")[i].style.display = "block";
 		  
  }
   function collapse(i){
-	 	 document.getElementsByClassName("detail-data")[i].style.display = "none";
-		 document.getElementsByClassName("right-content-collapse")[i].style.display = "none";
-		  document.getElementsByClassName("right-content")[i].style.display = "block";
+		document.getElementsByClassName("detail-data")[i].style.display = "none";
+		document.getElementsByClassName("right-content-collapse")[i].style.display = "none";
+		document.getElementsByClassName("right-content")[i].style.display = "block";
  }
  
- function getname(){
+ function getname(i,url){
 	 var xhttp = new  XMLHttpRequest();
 	 xhttp.onreadystatechange= function(){
 		 if(xhttp.readyState == 4 && xhttp.status == 200){
-			 console.log("successfull::"+key+"::"+xhttp.responseText);
+			 //console.log("successfull::"+key+"::"+xhttp.responseText);
 			 var htmlData = "",JsonData=[];
 			 var parseJson = JSON.parse(xhttp.responseText);
-			 var detail = getname();
-			 for(var x in parseJson){
-				 JsonData[x] = {};
-				// JsonData[x].name = getname();
-				// JsonData[x].name = getname();
-			 }
+			
+				JsonData = {};
+				JsonData.name = parseJson['name'];
+				JsonData.company = parseJson['company'];
+				JsonData.location = "dsm";//parseJson['location'];
+				JsonData.followers = parseJson['followers'];
+				JsonData.following = parseJson['following'];
+				JsonData.public_gists = parseJson['public_gists'];
+				JsonData.public_repos = parseJson['public_repos'];
+				JsonData.profile_url = parseJson['profile_url'];
+				JsonData.blog = parseJson['blog'];
+				JsonData.email = parseJson['email'];
+				console.log("JsonData 1 :: "+JSON.stringify(JsonData));
+				
+				var htmlCont = '<div class="detail-data-row"><p>Location</p><p>'+JsonData.location+'</p></div><div class="detail-data-row"> <p>Public Repos</p><p>'+JsonData.public_repos+'</p></div><div class="detail-data-row"> <p>Public Gists</p><p>'+JsonData.public_gists+'</p></div><div class="detail-data-row"> <p>Followers</p><p>'+JsonData.followers+'</p></div><div class="detail-data-row"> <p>Public Gists</p><p>'+JsonData.following+'</p></div>'
+				document.getElementsByClassName('detail-data')[i].innerHTML = htmlCont;
+				//return JsonData;
+			 
+		 }else{
+			 return {};
 		 }
 	 }
-	xhttp.open('GET','https://api.github.com/search/users?q='+key,true);
+	xhttp.open('GET',url,true);
 	xhttp.setRequestHeader('Origin',"https://api.github.com");
 	xhttp.setRequestHeader('Accept','application/json');
 	xhttp.setRequestHeader('Content-Type','application/json');
@@ -40,16 +55,22 @@
 	 xhttp.onreadystatechange= function(){
 		 if(xhttp.readyState == 4 && xhttp.status == 200){
 			 console.log("successfull::"+key+"::"+xhttp.responseText);
-			 var htmlData = "",JsonData=[];
-			 var parseJson = JSON.parse(xhttp.responseText);
+			 var htmlData = "";
+			 var JsonData=[];
+			 var parseJson = (JSON.parse(xhttp.responseText)).items;
 			 
 			 for(var x in parseJson){
-				// var detail = this.getname();
-				 JsonData[x] = {};
-				// JsonData[x].name = getname();
-				// JsonData[x].name = getname();
+				JsonData[x] = {};
+				JsonData[x].login = parseJson[x].login   //getname(parseJson[x].url) ;
+				JsonData[x].html_url = parseJson[x].html_url
+				//JsonData[x].location = parseJson[x].location
+				JsonData[x].score = parseJson[x].score;
+				JsonData[x].url = parseJson[x].url;
+				console.log("JsonData -- :: "+x+"::"+JSON.stringify(JsonData));
+				
 			 }
-			 var JsonData = [{
+			 console.log("JsonData :: "+JSON.stringify(JsonData));
+			 /* var JsonData = [{
 						"name" : "nikita",
 						"profile_url" : "www.google.com",
 						"blog" : "http://tonsky.me/",
@@ -73,16 +94,19 @@
 						"public_gists": 24,
 						"followers": 1865,
 						"following": 39	}
-			 ];
+			 ]; */
 			 for(var i in JsonData){
-				 htmlData += '<div class="row"><div class="first-row"><div class="left-content"><img src="niki.jpg" id="profile-icon"></img></div><div class="mid-content"><p class="name">'+JsonData[i].name+'</p><p>Profile URL:'+JsonData[i].profile_url+' </p><p>Location:'+JsonData[i].location+' </p><p>Public Repos:'+JsonData[i].public_repos+
-				 ' </p></div><div class="right-content"><button id = "btn-detatil"'+i+' onclick="detail('+i+')">Details</button></div><div class="right-content-collapse"><button id = "btn-detatil"'+i+' onclick="collapse('+i+')">Collapse</button></div></div><div class="detail-data"'+i+'><div class="detail-data-row"><p>Location</p><p>'+JsonData[i].location+'</p></div><div class="detail-data-row"> <p>Public Repos</p><p>'+JsonData[i].public_repos+'</p></div><div class="detail-data-row"> <p>Public Gists</p><p>'+JsonData[i].public_gists+'</p></div><div class="detail-data-row"> <p>Followers</p><p>'+JsonData[i].followers+'</p></div><div class="detail-data-row"> <p>Public Gists</p><p>'+JsonData[i].following+'</p></div></div></div>';
+				/* htmlData += '<div class="row"><div class="first-row"><div class="left-content"><img src="niki.jpg" id="profile-icon"></img></div><div class="mid-content"><p class="name">'+JsonData[i].login+'</p><p>Profile URL:'+JsonData[i].html_url+' </p><p>Score:'+JsonData[i].score+' </p>'+
+				 ' </div><div class="right-content"><button id = "btn-detatil"'+i+' onclick="detail('+i+','+JsonData[i].url+')">Details</button></div><div class="right-content-collapse"><button id = "btn-detatil"'+i+' onclick="collapse('+i+')">Collapse</button></div></div><div class="detail-data"'+i+'><div class="detail-data-row"><p>Location</p><p>'+JsonData[i].location+'</p></div><div class="detail-data-row"> <p>Public Repos</p><p>'+JsonData[i].public_repos+'</p></div><div class="detail-data-row"> <p>Public Gists</p><p>'+JsonData[i].public_gists+'</p></div><div class="detail-data-row"> <p>Followers</p><p>'+JsonData[i].followers+'</p></div><div class="detail-data-row"> <p>Public Gists</p><p>'+JsonData[i].following+'</p></div></div></div>';*/
+				 
+				 htmlData += '<div class="row"><div class="first-row"><div class="left-content"><img src="niki.jpg" id="profile-icon"></img></div><div class="mid-content"><p class="name">'+JsonData[i].login+'</p><p>Profile URL:'+JsonData[i].html_url+' </p><p>Score:'+JsonData[i].score+' </p>'+
+				 ' </div><div class="right-content"><button id = "btn-detatil"'+i+' onclick="detail('+i+',\''+JsonData[i].url+'\')">Details</button></div><div class="right-content-collapse"><button id = "btn-detatil"'+i+' onclick="collapse('+i+')">Collapse</button></div></div><div class="detail-data"'+i+'></div></div>';
 			 }
 			 document.getElementsByClassName('row-container')[0].innerHTML =  htmlData;
-			 //function getName();
-			 //function getIcon();
+			 
 			 
 		 }
+		 
 	 }
 	
 	xhttp.open('GET','https://api.github.com/search/users?q='+key,true);
